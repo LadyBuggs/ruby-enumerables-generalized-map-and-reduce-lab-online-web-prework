cd6ff0438e3a60140ee7ce761fa5c0657cb7801b
Kathy B <kathybuggs@gmail.com>
2019-09-22 00:08:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+require "pry"
+
+def map(array)
+  new_array = []
+  i = 0
+  while i < array.length do
+    new_array << yield(array[i])
+    i += 1 
+  end
+  new_array
+end 
+
+def reduce(array, sv = nil)
+  value = 0 
+  i = 0 
+  while i < array.length do
+    if value.to_i
+    value = value + array[i]
+    i += 1 
+  else
+    i += 1 
+  end 
+  end
+  value
+end 
